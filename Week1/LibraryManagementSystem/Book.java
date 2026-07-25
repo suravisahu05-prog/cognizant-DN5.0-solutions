0dcf73acbb733da66f32d78923b959016b338746
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-25 23:21:20 +0530

    Create Book.java

---
+++ Week1/LibraryManagementSystem/Book.java
@@ -0,0 +1,19 @@
+public class Book {
+    private int bookId;
+    private String title;
+    private String author;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+    }
+
+    public int getBookId() { return bookId; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+
+    public String toString() {
+        return "Book{id=" + bookId + ", title=" + title + ", author=" + author + "}";
+    }
+}
