cfed495220b28e7569cc75c73fcf86b273207bab
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 01:14:43 +0530

    Create PaymentGateway.java

---
+++ Week2/ArgumentMatchingExample/PaymentGateway.java
@@ -0,0 +1,6 @@
+public class PaymentGateway {
+    public String processPayment(String customerId, double amount) {
+        // In real life, this would call a real payment provider
+        return "Processed";
+    }
+}
