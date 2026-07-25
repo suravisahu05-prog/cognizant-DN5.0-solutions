67279caaf02d71f5db32b72b927b755ab436f29d
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 01:09:47 +0530

    Create ExternalApi.java

---
+++ Week2/MockingStubbingExample/ExternalApi.java
@@ -0,0 +1,6 @@
+public class ExternalApi {
+    public String getData() {
+        // In real life, this would call an actual external service (network call, etc.)
+        return "Real Data";
+    }
+}
