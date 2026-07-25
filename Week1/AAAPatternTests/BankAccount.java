9c4b13a335293516c9f257fd615c929de0a44f41
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 00:36:51 +0530

    Create BankAccount.java

---
+++ Week1/AAAPatternTests/BankAccount.java
@@ -0,0 +1,22 @@
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient balance");
+        }
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
