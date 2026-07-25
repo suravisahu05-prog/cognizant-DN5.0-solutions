87c7b3c92f12dfbb91c7976fba3355e1488c1bca
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 02:19:19 +0530

    Create UserService.java

---
+++ Week2/ServiceExceptionHandlingExample/UserService.java
@@ -0,0 +1,18 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
