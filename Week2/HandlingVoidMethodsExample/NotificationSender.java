b10bee5f2520c16d0ca8216a8eded6e308213cdd
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 01:17:18 +0530

    Create NotificationSender.java

---
+++ Week2/HandlingVoidMethodsExample/NotificationSender.java
@@ -0,0 +1,6 @@
+public class NotificationSender {
+    public void sendNotification(String message) {
+        // In real life, this might send an actual email/SMS
+        System.out.println("Sending: " + message);
+    }
+}
