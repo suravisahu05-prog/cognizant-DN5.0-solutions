7075eeb016dffef2bbe62b90ef6fa5761ce1fd81
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 01:22:45 +0530

    Create PaymentProcessor.java

---
+++ Week2/VerifyingInteractionOrderExample/PaymentProcessor.java
@@ -0,0 +1,13 @@
+public class PaymentProcessor {
+    public void validatePayment() {
+        System.out.println("Validating payment...");
+    }
+
+    public void chargeCustomer() {
+        System.out.println("Charging customer...");
+    }
+
+    public void sendReceipt() {
+        System.out.println("Sending receipt...");
+    }
+}
