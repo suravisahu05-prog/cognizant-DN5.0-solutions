7b9522a4dafff819772c1d9514aaf681305f5720
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 13:57:53 +0530

    Create ResilientGatewayDemoApplication.java

---
+++ Week4/ResilientGatewayDemo/src/main/java/com/cognizant/resilientgateway/ResilientGatewayDemoApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.resilientgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilientGatewayDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ResilientGatewayDemoApplication.class, args);
+    }
+}
