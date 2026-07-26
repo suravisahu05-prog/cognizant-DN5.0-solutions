f95ada37ad8bebd7d891a047a4a4241d5a524cb8
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 13:29:43 +0530

    Create SecurityConfig.java

---
+++ Week4/CentralAuthDemo/src/main/java/com/cognizant/auth/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.cognizant.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2.loginPage("/oauth2/authorization/my-client"));
+
+        return http.build();
+    }
+}
