ddc7d4d04157225abaa7d083b6a2d025774afe4c
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 13:49:31 +0530

    Create EdgeGatewayDemoApplication.java

---
+++ Week4/EdgeGatewayDemo/src/main/java/com/cognizant/gateway/EdgeGatewayDemoApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeGatewayDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeGatewayDemoApplication.class, args);
+    }
+}
