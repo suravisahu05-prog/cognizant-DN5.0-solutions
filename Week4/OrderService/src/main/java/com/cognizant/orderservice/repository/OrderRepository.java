b9902ed13da2d57b8f798df73115f330a113fc5e
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 14:05:04 +0530

    Create OrderRepository.java

---
+++ Week4/OrderService/src/main/java/com/cognizant/orderservice/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.cognizant.orderservice.repository;
+
+import com.cognizant.orderservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
