80c25a52b03137360720a1eaba177049d1dfe387
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 11:06:46 +0530

    Create UserService.java

---
+++ Week2/MockingRepositoryInServiceExample/UserService.java
@@ -0,0 +1,13 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
