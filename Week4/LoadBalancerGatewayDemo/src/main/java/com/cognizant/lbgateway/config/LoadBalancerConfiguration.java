2e08f2442d9c3057149e2f08b604c6eed2bc7e0f
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 13:53:58 +0530

    Create LoadBalancerConfiguration.java

---
+++ Week4/LoadBalancerGatewayDemo/src/main/java/com/cognizant/lbgateway/config/LoadBalancerConfiguration.java
@@ -0,0 +1,25 @@
+package com.cognizant.lbgateway.config;
+
+import com.alibaba.cloud.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.RandomLoadBalancer;
+import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
+import org.springframework.cloud.loadbalancer.support.LoadBalancerClientFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+@Configuration
+public class LoadBalancerConfiguration {
+
+    @Bean
+    public ReactorLoadBalancer<ServiceInstance> randomLoadBalancer(
+            Environment environment, LoadBalancerClientFactory loadBalancerClientFactory) {
+        String name = environment.getProperty(LoadBalancerClientFactory.PROPERTY_NAME);
+        return new RandomLoadBalancer(
+                loadBalancerClientFactory.getLazyProvider(name, ServiceInstanceListSupplier.class),
+                name);
+    }
+}
