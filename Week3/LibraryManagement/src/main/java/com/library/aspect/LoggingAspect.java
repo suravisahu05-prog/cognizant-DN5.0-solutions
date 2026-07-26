7d4e4171a44b0db0a9834aa4c0d75981595da2f1
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 12:41:54 +0530

    Update LoggingAspect.java

--- Week3/LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
+++ Week3/LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -1,11 +1,24 @@
 package com.library.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LoggingAspect {
+
+    @Before("execution(* com.library.service.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Before method: " + joinPoint.getSignature());
+    }
+
+    @After("execution(* com.library.service.*.*(..))")
+    public void logAfter(JoinPoint joinPoint) {
+        System.out.println("After method: " + joinPoint.getSignature());
+    }
 
     @Around("execution(* com.library.service.*.*(..))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
