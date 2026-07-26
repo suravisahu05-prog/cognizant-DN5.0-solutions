89aa6794fec071bff1c27efc8f381ea974c50a0b
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 13:53:35 +0530

    Create LoadBalancerGatewayDemoApplication.java

---
+++ Week4/LoadBalancerGatewayDemo/src/main/java/com/cognizant/lbgateway/LoadBalancerGatewayDemoApplication.java
@@ -0,0 +1,11 @@
+package com.cognizant.lbgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancerGatewayDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancerGatewayDemoApplication.class, args);
+    }
+}
