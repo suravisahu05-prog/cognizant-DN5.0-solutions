40bbe2561a0fbad04430dc1280b750d542f3d7eb
suravisahu05-prog <suravisahu05@gmail.com>
2026-07-26 12:40:12 +0530

    Update BookService.java

--- Week3/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week3/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,20 +1,24 @@
 package com.library.service;
 
 import com.library.repository.BookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
 public class BookService {
 
     private BookRepository bookRepository;
+    private String libraryName;
 
-    @Autowired
+    // Constructor Injection
+    public BookService(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    // Setter Injection
     public void setBookRepository(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
     }
 
     public void addBook(String title) {
+        System.out.println("Library: " + libraryName);
         bookRepository.save(title);
     }
 }
